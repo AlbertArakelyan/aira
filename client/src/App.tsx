500010d15e018d13d9a6f3f0b6a994423b077609
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-04-30 22:49:25 +0400

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App text-primary-400">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    colors: {
+      primary: colors.violet,
+      secondary: colors.fuchsia,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
