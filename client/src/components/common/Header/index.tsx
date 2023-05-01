3ae1508237e9f2a722f6bb827cbb951c08641a1c
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-05-01 19:57:02 +0400

    made header colors more stylish

--- client/src/components/common/Header/index.tsx
+++ client/src/components/common/Header/index.tsx
@@ -5,7 +5,7 @@ import { Avatar } from '../../index';
 const Header = () => {
   return (
     <header className="px-4 py-3 border-solid border-b-[1px] border-gray-200 flex justify-end items-center">
-      <div className="flex justify-end items-center">
+      <div className="flex justify-end items-center text-slate-700">
         <span className="mr-3">Albert Arakelyan</span>
         <Avatar />
       </div>
