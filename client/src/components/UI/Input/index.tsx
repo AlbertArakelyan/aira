66fedfdc700262f229544f219d61f5c961c91a13
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-05-05 06:18:13 +0400

    styled Input component

--- client/src/components/UI/Input/index.tsx
+++ client/src/components/UI/Input/index.tsx
@@ -5,14 +5,12 @@ import { IInputProps } from './types';
 
 
 const Input: FC<IInputProps> = ({
-  color = 'primary',
-  shade = '500',
   error,
   className = '',
   ...props
 }) => {
   return (
-    <input className={`border border-${color}-${shade} rounded outline-none p-2 ${className}`} {...props} />
+    <input className={`border border-gray-500 focus:border-primary-500 transition focus:ring-primary-500 focus:ring-2 rounded outline-none p-2 ${className}`} {...props} />
   );
 };
 
