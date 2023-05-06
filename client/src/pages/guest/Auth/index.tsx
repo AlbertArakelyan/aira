468a3f533a6c17527a140e74c15119d020d48beb
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-05-06 12:31:25 +0400

    added sign up form

--- client/src/pages/guest/Auth/index.tsx
+++ client/src/pages/guest/Auth/index.tsx
@@ -7,6 +7,9 @@ import { Input, Button } from '../../../components';
 const Auth = () => {
   const [isSignUp, setIsSugnUp] = useState(false);
 
+  const handleToggleIsSignUp = () => {
+    setIsSugnUp((prevIsSignUp) => !prevIsSignUp);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
@@ -17,22 +20,29 @@ const Auth = () => {
         <Input
           className="w-full mb-3"
           placeholder="Email address"
+          type="email"
         />
         <Input
-          className="w-full mb-5"
+          className={`w-full ${isSignUp ? 'mb-3' : 'mb-5'}`}
           placeholder="Password"
+          type="password"
         />
+        {isSignUp && (<Input
+          className="w-full mb-5"
+          placeholder="Confirm password"
+          type="password"
+        />)}
         <Button className="w-full mb-4">
-          Sign In
+          Sign {isSignUp ? 'Up' : 'In'}
         </Button>
         <div className="text-sm">
           {isSignUp ? (
             <p>
-              Already have an account? <span className="text-primary-400 cursor-pointer">Sign In</span>
+              Already have an account? <span className="text-primary-400 cursor-pointer" onClick={handleToggleIsSignUp}>Sign In</span>
             </p>
           ) : (
             <p>
-              Don't have an account? <span className="text-primary-400 cursor-pointer">Sign Up</span>
+              Don't have an account? <span className="text-primary-400 cursor-pointer" onClick={handleToggleIsSignUp}>Sign Up</span>
             </p>
           )}
         </div>
